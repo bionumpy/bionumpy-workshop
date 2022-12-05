bf9aa0473121ebd3b8f36aa32b1b7ec86dd5731c
Ivar Grytten <ivar.grytten@gmail.com>
2022-12-05 11:59:14 +0100

    new chipseq task

---
+++ livekoding.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+sequences = np.array([
+    [0, 1, 2, 3],
+    [10, 11, 12, 13],
+    [1, 1, 1, 1]
+])
+
+
+mask = sequences < 10
+
+print(np.sum(mask, axis=0))
+
+https://github.com/uio-bmi/bionumpy-workshop
+
+
